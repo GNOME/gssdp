172025f7f122f78ed413aaba9b3189e9c335b1d9
Jens Georg <jensg@openismus.com>
2013-06-06 11:29:20 +0200

    Use SO_REUSEPORT on Debian/kFreeBSD
    Same as e4c8a58928d2fc94bc9b7eb6f6ef5115f7dd6f06 but for Debian on FreeBSD
    kernels where __FreeBSD__ is not defined.

--- libgssdp/gssdp-socket-functions.c
+++ libgssdp/gssdp-socket-functions.c
@@ -159,10 +159,10 @@ gboolean
 gssdp_socket_reuse_address (GSocket *socket,
                             gboolean enable,
                             GError **error) {
-#if defined(G_OS_WIN32) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__FreeBSD__)
+#if defined(G_OS_WIN32) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
         return gssdp_socket_option_set (socket,
                                         SOL_SOCKET,
-#if defined(__OpenBSD__) || defined (__NetBSD__) || defined (__FreeBSD__)
+#if defined(__OpenBSD__) || defined (__NetBSD__) || defined (__FreeBSD__) || defined(__FreeBSD_kernel__)
                                         SO_REUSEPORT,
 #else
                                         SO_REUSEADDR,
