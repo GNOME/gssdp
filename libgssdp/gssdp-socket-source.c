GNOME/gssdp

commit e386c0de7bd33f7bd3371da29ce48e5508e66698

SocketSource: Refactor init function
Split in smaller functions to reduce complexity

libgssdp/gssdp-socket-source.c
@@ -83,6 +83,212 @@ gssdp_socket_source_initable_init (gpointer               g_iface,
         iface->init = gssdp_socket_source_do_init;
 }
 
+static GInetAddress *
+gssdp_socket_source_get_multicast_group (GSSDPSocketSourcePrivate *priv,
+                                         gboolean                 *is_link_local)
+{
+        if (g_inet_address_get_is_link_local (priv->address)) {
+                *is_link_local = TRUE;
+                return g_inet_address_new_from_string (SSDP_V6_LL);
+        }
+        return g_inet_address_new_from_string (SSDP_V6_SL);
+}
+
+static GSocketAddress *
+gssdp_socket_source_create_bind_address (GSSDPSocketSourcePrivate *priv,
+                                         GInetAddress             *group,
+                                         guint                     port,
+                                         gboolean                  is_link_local)
+{
+        if (group) {
+                /* Multicast bind */
+#ifdef G_OS_WIN32
+                return g_inet_socket_address_new (priv->address, port);
+#else
+                return g_object_new (G_TYPE_INET_SOCKET_ADDRESS,
+                                     "address", group,
+                                     "port", port,
+                                     "scope-id", priv->index,
+                                     NULL);
+#endif
+        }
+
+        /* Unicast bind */
+        if (is_link_local) {
+                return g_object_new (G_TYPE_INET_SOCKET_ADDRESS,
+                                     "address", priv->address,
+                                     "port", port,
+                                     "scope-id", priv->index,
+                                     NULL);
+        }
+        return g_inet_socket_address_new (priv->address, port);
+}
+
+static gboolean
+gssdp_socket_source_create_socket (GSSDPSocketSourcePrivate *priv,
+                                   GSocketFamily             family,
+                                   GError                  **error)
+{
+        GError *inner_error = NULL;
+
+        priv->socket = g_socket_new (family,
+                                     G_SOCKET_TYPE_DATAGRAM,
+                                     G_SOCKET_PROTOCOL_UDP,
+                                     &inner_error);
+
+        if (!priv->socket) {
+                g_propagate_prefixed_error (error,
+                                            inner_error,
+                                            "Could not create socket");
+                return FALSE;
+        }
+
+        /* Enable broadcasting */
+        g_socket_set_broadcast (priv->socket, TRUE);
+
+        if (!gssdp_socket_enable_info (priv->socket,
+                                       family,
+                                       TRUE,
+                                       &inner_error)) {
+                g_propagate_prefixed_error (error,
+                                            inner_error,
+                                            "Failed to enable info messages");
+                return FALSE;
+        }
+
+        /* TTL */
+        if (priv->ttl == 0) {
+                /* UDA/1.0 says 4, UDA/1.1 says 2 */
+                priv->ttl = 4;
+                if (family == G_SOCKET_FAMILY_IPV6) {
+                        /* UDA 2.0, Annex A says 10 hops */
+                        priv->ttl = 10;
+                }
+        }
+
+        g_socket_set_multicast_ttl (priv->socket, priv->ttl);
+
+        return TRUE;
+}
+
+static gboolean
+gssdp_socket_source_configure_socket (GSSDPSocketSourcePrivate *priv,
+                                      GSocketFamily             family,
+                                      GError                  **error)
+{
+        GError *inner_error = NULL;
+
+        if (priv->type == GSSDP_SOCKET_SOURCE_TYPE_MULTICAST) {
+                /* Enable multicast loopback */
+                g_socket_set_multicast_loopback (priv->socket, TRUE);
+        } else {
+                if (family != G_SOCKET_FAMILY_IPV6 ||
+                    (!g_inet_address_get_is_loopback (priv->address))) {
+
+                        if (!gssdp_socket_mcast_interface_set (priv->socket,
+                                                priv->address,
+                                                (guint32) priv->index,
+                                                &inner_error)) {
+                                g_propagate_prefixed_error (
+                                                error,
+                                                inner_error,
+                                                "Failed to set multicast interface");
+
+                                return FALSE;
+                        }
+                }
+        }
+
+        return TRUE;
+}
+
+static gboolean
+gssdp_socket_source_bind_socket (GSSDPSocketSourcePrivate *priv,
+                                 GSocketFamily             family,
+                                 GSocketAddress           *bind_address,
+                                 GError                  **error)
+{
+        GError *inner_error = NULL;
+
+        /* Normally g_socket_bind does this, but it is disabled on
+         * windows since SO_REUSEADDR has different semantics
+         * there, also we nees SO_REUSEPORT on OpenBSD. This is a nop
+         * everywhere else.
+         */
+        if (!gssdp_socket_reuse_address (priv->socket,
+                                         TRUE,
+                                         &inner_error)) {
+                g_propagate_prefixed_error (
+                                error,
+                                inner_error,
+                                "Failed to enable reuse");
+
+                return FALSE;
+        }
+
+        /* Bind to requested port and address */
+        if (!g_socket_bind (priv->socket,
+                            bind_address,
+                            TRUE,
+                            &inner_error)) {
+                g_propagate_prefixed_error (error,
+                                            inner_error,
+                                            "Failed to bind socket");
+
+                return FALSE;
+        }
+
+        return TRUE;
+}
+
+static void
+gssdp_socket_source_get_assigned_port (GSSDPSocketSourcePrivate *priv,
+                                       GError                  **error)
+{
+        GError *inner_error = NULL;
+        GSocketAddress *addr =
+                g_socket_get_local_address (priv->socket, &inner_error);
+
+        if (inner_error != NULL) {
+                g_propagate_prefixed_error (
+                        error,
+                        inner_error,
+                        "Failed to get port from socket");
+        }
+
+        priv->port = g_inet_socket_address_get_port (
+                G_INET_SOCKET_ADDRESS (addr));
+        g_object_unref (addr);
+}
+
+static gboolean
+gssdp_socket_source_join_multicast_group (GSSDPSocketSourcePrivate *priv,
+                                          GInetAddress             *group,
+                                          GError                  **error)
+{
+        GError *inner_error = NULL;
+
+        /* The 4th argument 'iface_name' can't be NULL even though Glib API doc says you
+         * can. 'NULL' will fail the test.
+         */
+        if (!g_socket_join_multicast_group (priv->socket,
+                                            group,
+                                            FALSE,
+                                            priv->device_name,  /*   e.g. 'lo' */
+                                            &inner_error)) {
+                char *address = g_inet_address_to_string (group);
+                g_propagate_prefixed_error (error,
+                                            inner_error,
+                                            "Failed to join group %s",
+                                            address);
+                g_free (address);
+
+                return FALSE;
+        }
+
+        return TRUE;
+}
+
 static void
 gssdp_socket_source_get_property (GObject              *object,
                                   guint                 property_id,
@@ -181,9 +387,7 @@ gssdp_socket_source_do_init (GInitable                   *initable,
         GSSDPSocketSourcePrivate *priv = NULL;
         GSocketAddress *bind_address = NULL;
         GInetAddress *group = NULL;
-        GError *inner_error = NULL;
         GSocketFamily family;
-        gboolean success = FALSE;
         gboolean link_local = FALSE;
 
         self = GSSDP_SOCKET_SOURCE (initable);
@@ -193,194 +397,53 @@ gssdp_socket_source_do_init (GInitable                   *initable,
 
         if (family == G_SOCKET_FAMILY_IPV4)
                 group = g_inet_address_new_from_string (SSDP_ADDR);
-        else {
-                /* IPv6 */
-                /* According to Annex.A, we need to check the scope of the
-                 * address to use the proper multicast group */
-                if (g_inet_address_get_is_link_local (priv->address)) {
-                            group = g_inet_address_new_from_string (SSDP_V6_LL);
-                            link_local = TRUE;
-                } else {
-                            group = g_inet_address_new_from_string (SSDP_V6_SL);
-                }
-        }
+        else
+                group = gssdp_socket_source_get_multicast_group (priv, &link_local);
 
-
-        /* Create socket */
-        priv->socket = g_socket_new (family,
-                                     G_SOCKET_TYPE_DATAGRAM,
-                                     G_SOCKET_PROTOCOL_UDP,
-                                     &inner_error);
-
-        if (!priv->socket) {
-                g_propagate_prefixed_error (error,
-                                            inner_error,
-                                            "Could not create socket");
-
+        /* Create and configure socket */
+        if (!gssdp_socket_source_create_socket (priv, family, error))
                 goto error;
-        }
 
-        /* Enable broadcasting */
-        g_socket_set_broadcast (priv->socket, TRUE);
-
-        if (!gssdp_socket_enable_info (priv->socket,
-                                       family,
-                                       TRUE,
-                                       &inner_error)) {
-                g_propagate_prefixed_error (error,
-                                            inner_error,
-                                            "Failed to enable info messages");
-
+        /* Configure socket for type */
+        if (!gssdp_socket_source_configure_socket (priv, family, error))
                 goto error;
-        }
 
-        /* TTL */
-        if (priv->ttl == 0) {
-                /* UDA/1.0 says 4, UDA/1.1 says 2 */
-                priv->ttl = 4;
-                if (family == G_SOCKET_FAMILY_IPV6) {
-                        /* UDA 2.0, Annex A says 10 hops */
-                        priv->ttl = 10;
-                }
-        }
+        /* Get port for binding */
+        guint port = SSDP_PORT;
+        if (priv->type == GSSDP_SOCKET_SOURCE_TYPE_SEARCH)
+                port = priv->port;
 
-        g_socket_set_multicast_ttl (priv->socket, priv->ttl);
+        /* Create bind address */
+        bind_address = gssdp_socket_source_create_bind_address (priv, group, port, link_local);
 
+        /* Bind socket */
+        if (!gssdp_socket_source_bind_socket (priv, family, bind_address, error))
+                goto error;
 
-        /* Set up additional things according to the type of socket desired */
+        /* Get assigned port for SEARCH type */
+        if (priv->type == GSSDP_SOCKET_SOURCE_TYPE_SEARCH && priv->port == 0)
+                gssdp_socket_source_get_assigned_port (priv, error);
+
+        /* Join multicast group if needed */
         if (priv->type == GSSDP_SOCKET_SOURCE_TYPE_MULTICAST) {
-                /* Enable multicast loopback */
-                g_socket_set_multicast_loopback (priv->socket, TRUE);
-
-
-#ifdef G_OS_WIN32
-                bind_address = g_inet_socket_address_new (priv->address,
-                                                          SSDP_PORT);
-#else
-                bind_address = g_object_new (G_TYPE_INET_SOCKET_ADDRESS,
-                                             "address", group,
-                                             "port", SSDP_PORT,
-                                             "scope-id", priv->index,
-                                             NULL);
-#endif
-        } else {
-                guint port = SSDP_PORT;
-
-                if (family != G_SOCKET_FAMILY_IPV6 ||
-                    (!g_inet_address_get_is_loopback (priv->address))) {
-
-                        if (!gssdp_socket_mcast_interface_set (priv->socket,
-                                                priv->address,
-                                                (guint32) priv->index,
-                                                &inner_error)) {
-                                g_propagate_prefixed_error (
-                                                error,
-                                                inner_error,
-                                                "Failed to set multicast interface");
-
-                                goto error;
-                        }
-                }
-
-                /* Use user-supplied or random port for the socket source used
-                 * by M-SEARCH */
-                if (priv->type == GSSDP_SOCKET_SOURCE_TYPE_SEARCH)
-                        port = priv->port;
-
-                if (link_local) {
-                    bind_address = g_object_new (G_TYPE_INET_SOCKET_ADDRESS,
-                                                 "address", priv->address,
-                                                 "port", port,
-                                                 "scope-id", priv->index,
-                                                 NULL);
-                } else {
-                    bind_address = g_inet_socket_address_new (priv->address,
-                                                              port);
-                }
-
-        }
-
-        /* Normally g_socket_bind does this, but it is disabled on
-         * windows since SO_REUSEADDR has different semantics
-         * there, also we nees SO_REUSEPORT on OpenBSD. This is a nop
-         * everywhere else.
-         */
-        if (!gssdp_socket_reuse_address (priv->socket,
-                                         TRUE,
-                                         &inner_error)) {
-                g_propagate_prefixed_error (
-                                error,
-                                inner_error,
-                                "Failed to enable reuse");
-
-                goto error;
-        }
-
-        /* Bind to requested port and address */
-        if (!g_socket_bind (priv->socket,
-                            bind_address,
-                            TRUE,
-                            &inner_error)) {
-                g_propagate_prefixed_error (error,
-                                            inner_error,
-                                            "Failed to bind socket");
-
-                goto error;
-        }
-
-        if (priv->type == GSSDP_SOCKET_SOURCE_TYPE_SEARCH && priv->port == 0) {
-                GSocketAddress *addr =
-                        g_socket_get_local_address (priv->socket, &inner_error);
-
-                if (inner_error != NULL) {
-                    g_propagate_prefixed_error (
-                            error,
-                            inner_error,
-                            "Failed to get port from socket");
-                }
-
-                priv->port = g_inet_socket_address_get_port (
-                        G_INET_SOCKET_ADDRESS (addr));
-                g_object_unref (addr);
-        }
-
-        if (priv->type == GSSDP_SOCKET_SOURCE_TYPE_MULTICAST) {
-                /* The 4th argument 'iface_name' can't be NULL even though Glib API doc says you
-                 * can. 'NULL' will fail the test.
-                 */
-                if (!g_socket_join_multicast_group (priv->socket,
-                                                    group,
-                                                    FALSE,
-                                                    priv->device_name,  /*   e.g. 'lo' */
-                                                    &inner_error)) {
-                        char *address = g_inet_address_to_string (group);
-                        g_propagate_prefixed_error (error,
-                                                    inner_error,
-                                                    "Failed to join group %s",
-                                                    address);
-                        g_free (address);
-
+                if (!gssdp_socket_source_join_multicast_group (priv, group, error))
                         goto error;
-                }
         }
 
         priv->source = g_socket_create_source (priv->socket,
                                                G_IO_IN | G_IO_ERR,
                                                NULL);
-        success = TRUE;
+
+        return TRUE;
 
 error:
         g_clear_object (&bind_address);
         g_clear_object (&group);
 
-        if (!success)
-                /* Be aware that inner_error has already been free'd by
-                 * g_propagate_error(), so we cannot access its contents
-                 * anymore. */
-                if (error == NULL)
-                        g_warning ("Failed to create socket source");
+        if (error == NULL)
+                g_warning ("Failed to create socket source");
 
-        return success;
+        return FALSE;
 }
 
 GSocket *

commit 7ec7d5ccde18509afb38aa7ef3ee85fb2762b1b5

socket-source: Bind to unicast address is broken
Fix regression introduced by e386c0de7bd33f7bd3371da29ce48e5508e66698
that caused the client not to bind to the unicast address of the
interface

libgssdp/gssdp-socket-source.c
@@ -106,8 +106,8 @@ gssdp_socket_source_create_bind_address (GSSDPSocketSourcePrivate *priv,
                                          guint                     port,
                                          gboolean                  is_link_local)
 {
-        if (group) {
-                /* Multicast bind */
+        if (priv->type == GSSDP_SOCKET_SOURCE_TYPE_MULTICAST) {
+                /* Multicast bind - bind to multicast group address */
 #ifdef G_OS_WIN32
                 return g_inet_socket_address_new (priv->address, port);
 #else
@@ -119,7 +119,7 @@ gssdp_socket_source_create_bind_address (GSSDPSocketSourcePrivate *priv,
 #endif
         }
 
-        /* Unicast bind */
+        /* Unicast bind - bind to interface address */
         if (is_link_local) {
                 return g_object_new (G_TYPE_INET_SOCKET_ADDRESS,
                                      "address", priv->address,